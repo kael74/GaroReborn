77b6482da0e5813efcb50305db6bc6eaf6247d8a
rodrigo gutierrez <rodrigo gutierrez@rodrigo>
2017-07-07 00:26:35 -0500

    Detalle DAo y Usuario DAO

--- src/java/ga/spring/hiber/dao/DetalleDAO.java
+++ src/java/ga/spring/hiber/dao/DetalleDAO.java
@@ -5,10 +5,100 @@
  */
 package ga.spring.hiber.dao;
 
+import ga.spring.hiber.model.Detalle;
+import ga.spring.hiber.model.Usuario;
+import ga.spring.hiber.util.HibernateUtil;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+
 /**
  *
  * @author Computer
  */
-public class DetalleDAO {
+public class DetalleDAO implements OperacionTotal<Detalle>{
+
+    @Override
+    public int create(Detalle d) {
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        int x = 0;
+        try {
+            session.beginTransaction();
+            session.merge(d);
+            session.flush();
+            session.getTransaction().commit();
+            x = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return x;
+    }
+
+    @Override
+    public int update(Detalle d) {
+    
+        int x = 0;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Detalle d1 = new Detalle();
+        try {
+            session.beginTransaction();
+            session.update(d);
+            session.getTransaction().commit();
+            x = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return x;
+    }
+
+    @Override
+    public int delete(int id) {
+        int x = 0;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            Detalle u = (Detalle) session.get(Detalle.class, id);
+            session.delete(u);
+            session.getTransaction().commit();
+            x = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return x;
+    }
+
+    @Override
+    public List<Detalle> readAll() {
+    
+        List<Detalle> lista = new ArrayList<>();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            lista = session.createCriteria(Usuario.class).list();
+            int count = lista.size();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return lista;
+    }
+
+    @Override
+    public List<Detalle> buscar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/java/ga/spring/hiber/dao/UsuarioDAO.java
+++ src/java/ga/spring/hiber/dao/UsuarioDAO.java
@@ -6,37 +6,97 @@
 package ga.spring.hiber.dao;
 
 import ga.spring.hiber.model.Usuario;
+import ga.spring.hiber.util.HibernateUtil;
+import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.input.KeyCode;
+import org.hibernate.Session;
 
 /**
  *
  * @author rodrigo gutierrez
  */
-public class UsuarioDAO implements OperacionTotal<Usuario>{
+public class UsuarioDAO implements OperacionTotal<Usuario> {
 
     @Override
     public int create(Usuario d) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        int x = 0;
+        try {
+            session.beginTransaction();
+            session.merge(d);
+            session.flush();
+            session.getTransaction().commit();
+            x = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return x;
     }
 
     @Override
     public int update(Usuario d) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int x = 0;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Usuario u1 = new Usuario();
+        try {
+            session.beginTransaction();
+            session.update(d);
+            session.getTransaction().commit();
+            x = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return x;
+
     }
 
     @Override
     public int delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int x = 0;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            Usuario u = (Usuario) session.get(Usuario.class, id);
+            session.delete(u);
+            session.getTransaction().commit();
+            x = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return x;
     }
 
     @Override
     public List<Usuario> readAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Usuario> lista = new ArrayList<>();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            lista = session.createCriteria(Usuario.class).list();
+            int count = lista.size();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return lista;
     }
 
     @Override
     public List<Usuario> buscar(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
